d886b3c70a0d6e9636ceba58125985fae4ac14ad
Sumkor <sumkor@foxmail.com>
2020-10-21 15:45:32 +0800

    init jdk1.8.0_91

---
+++ src/test/java/com/sumkor/MyTest.java
@@ -0,0 +1,15 @@
+package com.sumkor;
+
+import org.junit.Test;
+
+/**
+ * @author Sumkor
+ * @since 2020/10/21
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+}
